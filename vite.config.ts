c524be8cabae414152feb924edbca20167bbc87b
Agustin <agustinmarcelomoya@gmail.com>
2024-09-21 19:19:14 +0100

    Functional react component library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
+
+import {peerDependencies} from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: './lib/index.ts',
+      name: 'test-ui',
+      fileName: format => `test-ui.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })

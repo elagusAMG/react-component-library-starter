446bbd2ed861492a4396d2aab858e27273deb9fa
Agustin <agustinmarcelomoya@gmail.com>
2024-09-22 19:09:47 +0100

    Change biomejs config

--- lib/components/Card/Card.stories.tsx
+++ lib/components/Card/Card.stories.tsx
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Card } from "./Card";
+import type { Meta, StoryObj } from '@storybook/react';
+import { Card } from './Card';
 
 const meta = {
-	title: "Example/Card",
-	component: Card,
-	tags: ["docsPage"],
-	argTypes: {
-		title: {
-			control: { type: "text" },
-		},
-		description: {
-			control: { type: "text" },
-		},
-	},
+  title: 'Example/Card',
+  component: Card,
+  tags: ['docsPage'],
+  argTypes: {
+    title: {
+      control: { type: 'text' },
+    },
+    description: {
+      control: { type: 'text' },
+    },
+  },
 } satisfies Meta<typeof Card>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-	args: {
-		title: "Card Title",
-		description: "This is a card",
-	},
+  args: {
+    title: 'Card Title',
+    description: 'This is a card',
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,43 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-import { peerDependencies } from "./package.json";
+import { peerDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		dts({
-			tsconfigPath: "./tsconfig.app.json",
-			exclude: ["**/*.stories.tsx", "**/*.test.tsx"],
-		}),
-		viteStaticCopy({
-			targets: [
-				{
-					src: "./tailwind-preset/index.js", // Path to your preset file
-					dest: "./tailwind-base", // Destination folder inside dist/
-					rename: "index.js",
-				},
-			],
-		}),
-	],
-	build: {
-		lib: {
-			entry: "./lib/index.ts",
-			name: "test-ui",
-			fileName: (format) => `test-ui.${format}.js`,
-			formats: ["es", "cjs", "umd"],
-		},
-		rollupOptions: {
-			external: Object.keys(peerDependencies),
-			output: {
-				globals: {
-					react: "React",
-					"react-dom": "ReactDOM",
-				},
-			},
-		},
-	},
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './tailwind-preset/index.js', // Path to your preset file
+          dest: './tailwind-base', // Destination folder inside dist/
+          rename: 'index.js',
+        },
+      ],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: './lib/index.ts',
+      name: 'test-ui',
+      fileName: (format) => `test-ui.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });

70b83da18490538c0a8f12e98340029b754920af
Agustin <agustinmarcelomoya@gmail.com>
2024-09-22 19:05:35 +0100

    Fix colours preset + biome formatting files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: '.ui',
-  prefix: 'ui-',
-  content: ['./lib/**/*.{ts,tsx}'],
-  presets: [require('./tailwind-preset')],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	important: ".ui",
+	prefix: "ui-",
+	content: ["./lib/**/*.{ts,tsx}"],
+	presets: [require("./tailwind-preset/index.js")],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };

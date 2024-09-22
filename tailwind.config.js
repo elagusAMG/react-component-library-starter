2438fa77bef1ce547ee4ec5b18b9fe31f574e071
Agustin <agustinmarcelomoya@gmail.com>
2024-09-22 18:33:43 +0100

    Import base theme/preset to taiwlind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   important: '.ui',
-  prefix: "ui-",
-  content: [
-    "./lib/**/*.{ts,tsx}",
-  ],
+  prefix: 'ui-',
+  content: ['./lib/**/*.{ts,tsx}'],
+  presets: [require('./tailwind-preset')],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
